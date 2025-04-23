72a78f0758f5d60aa0ba7580e3f44d28197e9b65
Jeff Ivan Mayor <jeffivanbiosanomayor@gmail.com>
2025-04-24 00:04:54 +0800

    Fix bug on recaptcha

--- form/forgot-password.php
+++ form/forgot-password.php
@@ -7,7 +7,6 @@
 
   session_start();
 
-
   use PHPMailer\PHPMailer\PHPMailer;
   use PHPMailer\PHPMailer\Exception;
 
--- form/login_validate.php
+++ form/login_validate.php
@@ -4,18 +4,23 @@ session_start();
 
 require '../includes/db.php';
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $recaptchaSecret = $_ENV['RECAPTCHA_SECREY_KEY'];
+    $recaptchaSecret = $_ENV['RECAPTCHA_SECRET_KEY'];
     $recaptchaResponse = $_POST['g-recaptcha-response'];
+
     $verify = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$recaptchaSecret}&response={$recaptchaResponse}");
     $captchaSuccess = json_decode($verify);
 
-    if(!$captchaSuccess){
+    if (!$captchaSuccess->success) {
         $_SESSION['error'] = "Captcha Verification Failed";
         header("Location: login.php");
         exit();
     }
-
 
     $username = $_POST['username'];
     $password = $_POST['password'];
